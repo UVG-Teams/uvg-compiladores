5fce02a6adaa89afa5ebfb7f9488d61e8f1aa912
LucaBia <gian.luca.99@hotmail.com>
2022-08-22 17:48:35 -0600

    Avances

--- Proyecto1/main.py
+++ Proyecto1/main.py
@@ -8,6 +8,7 @@ Francisco Rosal
 """
 
 import sys
+from tkinter import scrolledtext
 from antlr4 import *
 from build.yaplLexer import yaplLexer
 from build.yaplParser import yaplParser
@@ -17,7 +18,17 @@ from yaplErrorListener import yaplErrorListener
 import tkinter as tk
 from tkinter import *
 from tkinter.ttk import *
+from tkinter.filedialog import askopenfile 
 
+
+def open_file():
+    file_path = askopenfile(initialdir = "./input", mode='r', filetypes=[('YAPL Files', '*yapl'), ("all files", "*.*")])
+    if file_path is not None:
+        # pass
+        filename_splited = file_path.name.split("/")
+        filename_splited = filename_splited[len(filename_splited)-1]
+        archivo1_ = "./" + filename_splited
+        label_file_explorer.configure(text="./" + filename_splited)
 
 def main(argv):
     input = FileStream(argv[1])
@@ -53,8 +64,28 @@ def main(argv):
 if __name__ == '__main__':
     window = tk.Tk()
     window.title('Analizador Semántico')
-    window.geometry("1500x1000")
-    window.attributes('-fullscreen', True)
-    # print("\n width x height = %d x %d (in mm)\n" %(width, height))
+    # window.geometry("1500x1000")
+    # width= window.winfo_screenwidth()               
+    # height= window.winfo_screenheight()               
+    # window.geometry("%dx%d" % (width, height))
+    window.state('zoomed') 
+    # window.attributes('-fullscreen', True)
+
+    adharbtn = Button(
+        window, 
+        text ='Choose File', 
+        command = lambda:open_file()
+    ) 
+    label_file_explorer = tk.Label(window, text = " ", width = 20, height = 4, fg = "white")
+    text_area_code = scrolledtext.ScrolledText(window, width = 200, height = 40, font = ("Times New Roman",15), foreground = "white")
+    text_area_console = scrolledtext.ScrolledText(window, width = 100, height = 16, font = ("Times New Roman",15), foreground = "red")
+    text_area_symbolT = scrolledtext.ScrolledText(window, width = 100, height = 16, font = ("Times New Roman",15), foreground = "blue")
+
+    adharbtn.grid(row=0, column=1)
+    label_file_explorer.grid(row=0, column=2)
+    text_area_code.grid(column = 0, row = 1, columnspan=50, rowspan=100)
+    text_area_console.grid(column = 0, row = 160)
+    text_area_symbolT.grid(column = 1, row = 160)
+
     main(sys.argv)
     window.mainloop()
