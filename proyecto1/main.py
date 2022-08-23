27b7ff4e861b3035035ad5a9ab40b25264de6336
LucaBia <gian.luca.99@hotmail.com>
2022-08-23 02:04:52 -0600

    line number

--- proyecto1/main.py
+++ proyecto1/main.py
@@ -21,6 +21,27 @@ from tkinter.ttk import *
 from tkinter.filedialog import askopenfile
 
 
+# Class that writes line number in text widget
+class LineNumbers(tk.Text):
+    def __init__(self, master, text_widget, **kwargs):
+        super().__init__(master, **kwargs)
+ 
+        self.text_widget = text_widget
+        self.text_widget.bind('<KeyPress>', self.on_key_press)
+ 
+        self.insert(1.0, '1')
+        self.configure(state='disabled')
+ 
+    def on_key_press(self, event=None):
+        final_index = str(self.text_widget.index(tk.END))
+        num_of_lines = final_index.split('.')[0]
+        line_numbers_string = "\n".join(str(no + 1) for no in range(int(num_of_lines)))
+        width = len(str(num_of_lines))
+        
+        self.configure(state='normal', width=width+1 if int(num_of_lines) < 10 else width)
+        self.delete(1.0, tk.END)
+        self.insert(1.0, line_numbers_string)
+        self.configure(state='disabled')
 
 
 def open_file():
@@ -33,9 +54,7 @@ def open_file():
         label_file_explorer.configure(text="./" + filename_splited)
 
         content = file_path.read()
-        text_area_code.insert(tk.INSERT, "\n")
         text_area_code.insert(tk.INSERT, content, "\n")
-
 
 def run():
     with open('input/temp.yapl', 'w') as f:
@@ -89,10 +108,12 @@ def main(argv):
         print("-----------------------------------------------------------------")
         print("\n" + yaplErrorListener.ANSI_RESET)
 
+
+
 if __name__ == '__main__':
     window = tk.Tk()
     window.title('Analizador Semántico')
-    window.state('zoomed') 
+    window.state('zoomed')
     
 
     run_main = BooleanVar()
@@ -109,19 +130,32 @@ if __name__ == '__main__':
         command = run
     )
     label_file_explorer = tk.Label(window, text = " ", width = 20, height = 4, fg = "white")
-    text_area_code = scrolledtext.ScrolledText(window, width = 204, height = 40, font = ("Times New Roman",15), foreground = "white")
-    text_area_console = scrolledtext.ScrolledText(window, width = 101, height = 16, font = ("Times New Roman",15), foreground = "green")
-    text_area_symbolT = scrolledtext.ScrolledText(window, width = 99, height = 16, font = ("Times New Roman",15), foreground = "skyblue")
+    # text_area_code = scrolledtext.ScrolledText(window, width = 204, height = 40, font = ("Times New Roman",15), foreground = "white")
+    text_area_code = tk.Text(window, width=202, height=38, font=("Times New Roman", 15), foreground="white", highlightthickness=0)
+    text_area_console = tk.Text(window, width=102, height=16, font=("Times New Roman", 15), foreground="green", highlightthickness=0)
+    text_area_symbolT = tk.Text(window, width=99, height=16, font=("Times New Roman", 15), foreground="skyblue", highlightthickness=0)
+
+    l = LineNumbers(window, text_area_code, width=2, height=38, font=("Times New Roman", 15), foreground="gray", highlightthickness=0)
 
     # Add elements to UI
     adharbtn.grid(row=0, column=0)
     label_file_explorer.grid(row=0, column=1)
     runbtn.grid(row=0, column=19, columnspan=2)
-    text_area_code.grid(column=0, row=1, columnspan=20, rowspan=100, padx=(12, 0))
-    text_area_console.grid(column=0, row=166, columnspan=10)
-    text_area_symbolT.grid(column=10, row=166, columnspan=10)
+    text_area_code.grid(column=0, row=1, columnspan=20, rowspan=100, padx=(35, 0))
+    l.grid(column=0, row=1, padx=(0, 300))
+    text_area_console.grid(column=0, row=166, columnspan=10, pady=(20,0), padx=(0,4))
+    text_area_symbolT.grid(column=10, row=166, columnspan=10, pady=(20,0))
 
     runbtn.wait_variable(run_main)
     main(sys.argv)
     text_area_console.insert(tk.INSERT, "\nCool")
     window.mainloop()
+
+
+# if __name__ == '__main__':
+#     w = tk.Tk()
+#     t = tk.Text(w)
+#     l = LineNumbers(w, t, width=1)
+#     l.grid(column=0, row=0)
+#     t.grid(column=1, row=0)
+#     w.mainloop()
