d287ee8ba9c0a34b7273a91ef62a8bc63689f4da
FranciscoRosal <ros18676@uvg.edu.gt>
2022-11-26 17:00:00 -0600

    avances

--- proyecto3/output/code.s
+++ proyecto3/output/code.s
@@ -12,22 +12,29 @@
 	_b: .word 0
 	_t11: .asciiz "Hello, World.\n"
 	_t14: .asciiz "\n"
+	_t17: .word 1
+	_t18: .word 2
+	_t19: .word 0
+	_c: .word 0
 
 
 .text
 .globl main
 main:
 
+	#_t2 <- _t0 + _t1
 	lw $t1, _t0
 	lw $t2, _t1
 	add $t0, $t1, $t2
 	sw $t0, _a
 
+	#_t5 <- _t3 + _t4
 	lw $t1, _t3
 	lw $t2, _t4
 	add $t0, $t1, $t2
 	sw $t0, _t5
 
+	#_t8 <- _t6 * _t7
 	lw $t1, _t6
 	lw $t2, _t7
 	mul $t0, $t1, $t2
@@ -36,7 +43,9 @@ main:
 	# Movement
 	lw $t1, _t8
 	sw $t1, _t9
+	#_t9 <- _t8
 
+	#_t10 <- _t5 - _t9
 	lw $t1, _t5
 	lw $t2, _t9
 	sub $t0, $t1, $t2
@@ -59,6 +68,21 @@ main:
 
 	# Out Int
 	lw $a0, _b
+	li $v0, 1
+	syscall
+
+	#_t19 <- _t17 + _t18
+	lw $t1, _t17
+	lw $t2, _t18
+	add $t0, $t1, $t2
+	sw $t0, _t19
+
+	#_t20 <- _t19 # c
+	lw $t0, _t19
+	sw $t0, _c
+
+	# Out Int
+	lw $a0, _c
 	li $v0, 1
 	syscall
 
