ba428f1f9b5fe23d2992ae946245fb58fa153ac5
FranciscoRosal <ros18676@uvg.edu.gt>
2022-11-26 16:38:06 -0600

    avances

--- proyecto3/output/code.s
+++ proyecto3/output/code.s
@@ -18,50 +18,46 @@
 .globl main
 main:
 
-
-
 	lw $t1, _t0
 	lw $t2, _t1
 	add $t0, $t1, $t2
 	sw $t0, _a
-
-
 
 	lw $t1, _t3
 	lw $t2, _t4
 	add $t0, $t1, $t2
 	sw $t0, _t5
 
-
-
 	lw $t1, _t6
 	lw $t2, _t7
 	mul $t0, $t1, $t2
 	sw $t0, _t8
 
+	# Movement
 	lw $t1, _t8
 	sw $t1, _t9
-	#lw $t0, _t8
 
 	lw $t1, _t5
 	lw $t2, _t9
 	sub $t0, $t1, $t2
 	sw $t0, _b
 
-
+	# Out String
 	la $a0, _t11
 	li $v0, 4
 	syscall
 
+	# Out Int
 	lw $a0, _a
 	li $v0, 1
 	syscall
 
-
+	# Out String
 	la $a0, _t14
 	li $v0, 4
 	syscall
 
+	# Out Int
 	lw $a0, _b
 	li $v0, 1
 	syscall
